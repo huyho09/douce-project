4504617dee99b1219fcc24652b52eabbb24dfa5b
tttuan2703 <tttuan2703@gmail.com>
2025-02-16 23:37:16 +0700

    Integrate template to source

--- ScentifyWebApp/ScentifyWebApp/obj/Debug/net8.0/ScentifyWebApp.AssemblyInfo.cs
+++ ScentifyWebApp/ScentifyWebApp/obj/Debug/net8.0/ScentifyWebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ScentifyWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf68c345af28d0b23d33039545a7f5f14e6d33e8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6edb2b7b3e364f9912789f1e9d89d32f110b537")]
 [assembly: System.Reflection.AssemblyProductAttribute("ScentifyWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ScentifyWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
