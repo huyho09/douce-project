2f748ff9621685bebe1f4d71628689f871d21b70
Dang Le <dang.le@createstudios.com.au>
2025-02-19 17:56:28 +0700

    added the detail template

---
+++ ScentifyWebApp/ScentifyWebApp/Controllers/ProductDetailController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ScentifyWebApp.Controllers
+{
+	public class ProductDetailController : Controller
+	{
+		private readonly ILogger<ProductDetailController> _logger;
+		public ProductDetailController(ILogger<ProductDetailController> logger)
+		{
+			_logger = logger;
+		}
+
+		public IActionResult Index(int id = 0)
+		{
+			return View();
+		}
+	}
+}
