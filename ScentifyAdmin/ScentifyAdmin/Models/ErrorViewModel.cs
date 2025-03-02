2f8a5a9f46ed073dce5ef4ad5d832242011514cd
DESKTOP-9I2HJ3H\THOAIMSI <tttuan2703@gmail.com>
2025-03-02 11:24:41 +0700

    add admin source & export new db

---
+++ ScentifyAdmin/ScentifyAdmin/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ScentifyAdmin.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
