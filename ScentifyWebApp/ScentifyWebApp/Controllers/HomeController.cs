f880150956c54b7456430b5851a0b514d57fc040
dang.le <dang06072000@gmail.com>
2025-04-05 21:30:35 +0700

    updated the swiper perfume item in Home

--- ScentifyWebApp/ScentifyWebApp/Controllers/HomeController.cs
+++ ScentifyWebApp/ScentifyWebApp/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Newtonsoft.Json;
 using ScentifyWebApp.DAL.DB;
+using ScentifyWebApp.Libs;
 using ScentifyWebApp.Models.Dtos;
 using ScentifyWebApp.Models.Entities;
 using ScentifyWebApp.Models.ViewModels;
@@ -48,6 +49,31 @@ namespace ScentifyWebApp.Controllers
 					{
 						var dtoIngredients = JsonConvert.DeserializeObject<List<Ingredient>>(perfumes[i].Ingredients);
 						mappingList[i].DtoIngredients = dtoIngredients;
+					}
+					if (!string.IsNullOrEmpty(perfumes[i].PriceInfo))
+					{
+						var PriceInfoData = perfumes[i].PriceInfo;
+						var productSizes = JsonHelpers.ParseJson<ProductSize>(PriceInfoData);
+						if (productSizes != null && productSizes.Any())
+						{
+							mappingList[i].ProductSizes = productSizes;
+						}
+					}
+					if (!string.IsNullOrEmpty(perfumes[i].FragranceNotes))
+					{
+						var fragranceNotes = JsonHelpers.ParseJson<FragranceNote>(perfumes[i].FragranceNotes);
+						if (fragranceNotes != null && fragranceNotes.Any())
+						{
+							mappingList[i].Citrus = fragranceNotes[0].Citrus;
+							mappingList[i].Floral = fragranceNotes[0].Floral;
+							mappingList[i].Fruity = fragranceNotes[0].Fruity;
+							mappingList[i].Woody = fragranceNotes[0].Woody;
+							mappingList[i].Musky = fragranceNotes[0].Musky;
+							mappingList[i].Oriental = fragranceNotes[0].Oriental;
+							mappingList[i].Spicy = fragranceNotes[0].Spicy;
+							mappingList[i].Tobacco = fragranceNotes[0].Tobacco;
+							mappingList[i].Gourmand = fragranceNotes[0].Gourmand;
+						}
 					}
 				}
 
