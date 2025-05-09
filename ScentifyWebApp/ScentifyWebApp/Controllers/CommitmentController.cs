346d87d694e6bb19cb571008182a686db7783729
dang.le <dang06072000@gmail.com>
2025-05-09 11:46:13 +0700

    added the policy and Commitment page.

---
+++ ScentifyWebApp/ScentifyWebApp/Controllers/CommitmentController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ScentifyWebApp.Controllers
+{
+    public class CommitmentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ScentifyWebApp/ScentifyWebApp/Controllers/PolicyController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ScentifyWebApp.Controllers
+{
+    public class PolicyController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
