bd4fde93744d840a871da0f36890e2667bf53327
tuan.tai <tttuan2703@gmail.com>
2025-05-29 00:10:17 +0700

    export source

--- ScentifyAdmin/ScentifyAdmin/Models/Dtos/ProductSize.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ScentifyAdmin.Models.Dtos
-{
-	public class ProductSize
-	{
-		public int VolumeMl { get; set; }
-		public decimal Price { get; set; }
-		public string Currency { get; set; } = "VND";
-	}
-}
